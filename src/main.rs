53bc348f6f323f8ff5b36bd4432e8f14452b02ea
ItsHoff <ItsHoff@users.noreply.github.com>
2016-09-20 19:31:01 +0300

    Add hello triangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+#[macro_use]
+extern crate glium;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    pos: [f64; 2]
+}
+
+implement_vertex!(Vertex, pos);
+
 fn main() {
-    println!("When I grow up I'm gonna be a rendering engine!");
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    let v1 = Vertex{pos: [-0.5, 0.0]};
+    let v2 = Vertex{pos: [0.5, 0.5]};
+    let v3 = Vertex{pos: [0.5, -0.5]};
+    let shape = vec![v1, v2, v3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 330
+
+        in vec2 pos;
+
+        void main() {
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 330
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                    &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        for event in display.poll_events() {
+            match event {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
