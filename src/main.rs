0d887b2e8093c88c991bd8c93e821cb1eca3931f
ItsHoff <ItsHoff@users.noreply.github.com>
2016-09-21 20:07:17 +0300

    Implement glium tutorials

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate glium;
 
+mod teapot;
+
 #[derive(Copy, Clone)]
 struct Vertex {
     pos: [f64; 2]
@@ -10,43 +12,93 @@ implement_vertex!(Vertex, pos);
 
 fn main() {
     use glium::{DisplayBuild, Surface};
-    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    let display = glium::glutin::WindowBuilder::new().with_depth_buffer(24).build_glium().unwrap();
 
-    let v1 = Vertex{pos: [-0.5, 0.0]};
-    let v2 = Vertex{pos: [0.5, 0.5]};
-    let v3 = Vertex{pos: [0.5, -0.5]};
-    let shape = vec![v1, v2, v3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
+                                          &teapot::INDICES).unwrap();
 
     let vertex_shader_src = r#"
         #version 330
 
-        in vec2 pos;
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+
+        uniform mat4 matrix;
+        uniform mat4 perspective;
 
         void main() {
-            gl_Position = vec4(pos, 0.0, 1.0);
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 330
 
+        in vec3 v_normal;
+
         out vec4 color;
 
+        uniform vec3 u_light;
+
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = 0.5 * vec3(1.0);
+            vec3 regular_color = vec3(1.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
+    let params = glium::DrawParameters {
+        depth: glium::Depth {
+            test: glium::draw_parameters::DepthTest::IfLess,
+            write: true,
+            .. Default::default()
+        },
+        .. Default::default()
+    };
+
     loop {
+
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
-                    &Default::default()).unwrap();
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+            ]
+        };
+
+        let uniforms = uniform! {
+            matrix: [
+                [0.01, 0.0, 0.0, 0.0],
+                [0.0, 0.01, 0.0, 0.0],
+                [0.0, 0.0, 0.01, 0.0],
+                [0.0, 0.0, 2.0, 1.0f32]
+            ],
+            perspective: perspective,
+            u_light: [-1.0, 0.4, 0.9f32]
+        };
+
+        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+        target.draw((&positions, &normals), &indices, &program, &uniforms, &params).unwrap();
         target.finish().unwrap();
 
         for event in display.poll_events() {
