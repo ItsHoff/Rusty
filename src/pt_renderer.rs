2bdbc84f833fffca45c2fd4a4618d25903cccb6b
ItsHoff <ItsHoff@users.noreply.github.com>
2018-11-23 12:46:53 +0200

    Split coordinator to its own file

--- src/pt_renderer.rs
+++ src/pt_renderer.rs
@@ -1,6 +1,5 @@
 use std::path::Path;
 use std::sync::{
-    atomic::{AtomicUsize, Ordering},
     mpsc::{self, Receiver, Sender},
     Arc,
 };
@@ -17,66 +16,14 @@ use crate::stats;
 use crate::vertex::RawVertex;
 
 mod config;
+mod coordinator;
 mod render_worker;
 mod traced_image;
 
 pub use self::config::*;
+use self::coordinator::RenderCoordinator;
 use self::render_worker::RenderWorker;
 use self::traced_image::TracedImage;
-
-pub struct RenderCoordinator {
-    width: u32,
-    height: u32,
-    max_blocks: Option<usize>,
-    current_block: AtomicUsize,
-    block_width: u32,
-    block_height: u32,
-    x_blocks: usize,
-    y_blocks: usize,
-}
-
-impl RenderCoordinator {
-    fn new(width: u32, height: u32, config: &RenderConfig) -> RenderCoordinator {
-        let block_height = 50;
-        let block_width = 50;
-        let x_blocks = (f64::from(width) / f64::from(block_width)).ceil() as usize;
-        let y_blocks = (f64::from(height) / f64::from(block_height)).ceil() as usize;
-        let blocks_per_iter = x_blocks * y_blocks;
-        let max_blocks = config.max_iterations.map(|iters| iters * blocks_per_iter);
-        RenderCoordinator {
-            width,
-            height,
-            max_blocks,
-            current_block: AtomicUsize::new(0),
-            block_width,
-            block_height,
-            x_blocks,
-            y_blocks,
-        }
-    }
-
-    fn next_block(&self) -> Option<Rect> {
-        let block_i = self.current_block.fetch_add(1, Ordering::Relaxed);
-        if let Some(max) = self.max_blocks {
-            if block_i >= max {
-                return None;
-            }
-        };
-        let iter_i = block_i % (self.x_blocks * self.y_blocks);
-        let x_i = (iter_i % self.x_blocks) as u32;
-        let y_i = (iter_i / self.x_blocks) as u32;
-        let start_x = self.block_width * x_i;
-        let end_x = (self.block_width * (x_i + 1)).min(self.width);
-        let start_y = self.block_height * y_i;
-        let end_y = (self.block_height * (y_i + 1)).min(self.height);
-        Some(Rect {
-            left: start_x,
-            bottom: start_y,
-            width: end_x - start_x,
-            height: end_y - start_y,
-        })
-    }
-}
 
 struct PTVisualizer {
     shader: glium::Program,
